af80bda44485b34ffbd2c7eb31034180adbdd9f7
NKR <14344392+NKR00711@users.noreply.github.com>
2025-04-10 22:40:03 +0530

    Update trakt_infuse.js

--- gradle/wrapper/trakt_infuse.js
+++ gradle/wrapper/trakt_infuse.js
@@ -19,7 +19,7 @@ hostname = api.trakt.tv
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj = [{"key":"iapStatus_v2","value":"Ll/Dvdw/dCzWZev836yv+EYl+ayZXl8Rndw75KjLgpEqXwYAU6iwmR0XgB4lhhZDLzMIDHKjf5GrB/camzGfJk40o05Zod9M/08VW/SPucmMCbHf0PlezL+iwwbCJZY0xZwMoAw+fUougNUcb0TYGXXZc8+Lh7u5oVvlDvqLESe22mLi066CRFqOqwr0YtP+lp52SDqC7TxjSFdcKfO8Hw==","created_at":"3025-03-10T08:19:05.000Z","updated_at":"3025-03-10T14:19:52.000Z"}]
+obj = [{"key":"iapStatus_v2","value":"G5T3ZMnyYuNjUE6ZzTro37R7JgUJqlFS95sh6aDhiUL3MSzot4kngLFStzdazrHE1Q4+CgAEM6gFMIi9iaW5m8dXCj7dY78dCJJuMMFGDJUtFqXDMiotkO4ox3U2P/aFVUz2qTUxFA7tReQNci9Habx0398/dok/AoqGTauuYlFUcIIrFtJfVAS6BlB8OxvYjrJ83bzf47/TiPvzaDR5wQ==","created_at":"3025-03-10T08:19:05.000Z","updated_at":"3025-03-10T14:19:52.000Z"}]
 
 body = JSON.stringify(obj);
 $done({ body: body });
