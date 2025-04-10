845768ea5c629299d79c65ee9e36ed9782e4a326
NKR <14344392+NKR00711@users.noreply.github.com>
2025-04-10 22:30:46 +0530

    Create trakt_infuse_FreeIDMZone.js

---
+++ gradle/wrapper/trakt_infuse_FreeIDMZone.js
@@ -0,0 +1,43 @@
+/*************************************
+
+Project name: Infuse Trakt
+Download address: https://t.me/FreeIDMZoneC
+Update date: 2025-04-10
+Script author: NKR
+Telegram channel: https://t.cn/FreeIDMZoneC
+Usage statement: ⚠️For reference only, 🈲Reprint and sale!
+
+**************************************
+
+[rewrite_local]
+^https:\/\/api\.trakt\.tv\/users\/settings url script-response-body https://raw.githubusercontent.com/NKR00711/Virtual-Hosts/refs/heads/master/gradle/wrapper/trakt_infuse_FreeIDMZone.js
+
+[mitm]
+hostname = api.trakt.tv
+
+*************************************/
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj["vip"] = true;
+
+obj["user"] = obj["user"] || {}; // Ensure the user object exists
+obj["user"]["vip_og"] = true;
+obj["user"]["vip_years"] = 99999;
+obj["user"]["ids"] = {
+    "uuid": "1f33e302422c8e6c04189b79e373dedbf25fe0a4",
+    "slug": "freeidmzone_user_1"
+};
+obj["user"]["username"] = "FreeIDMZone_User_1";
+obj["user"]["vip"] = true;
+
+body = JSON.stringify(obj);
+$done({ body: body });
+
+// var ddm = JSON.parse($response.body);
+
+// ddm=[{"key":"iapStatus_v2","value":"Ll/Dvdw/dCzWZev836yv+EYl+ayZXl8Rndw75KjLgpEqXwYAU6iwmR0XgB4lhhZDLzMIDHKjf5GrB/camzGfJk40o05Zod9M/08VW/SPucmMCbHf0PlezL+iwwbCJZY0xZwMoAw+fUougNUcb0TYGXXZc8+Lh7u5oVvlDvqLESe22mLi066CRFqOqwr0YtP+lp52SDqC7TxjSFdcKfO8Hw==","created_at":"3025-03-10T08:19:05.000Z","updated_at":"3025-03-10T14:19:52.000Z"}];
+
+// $done({body : JSON.stringify(ddm)});
+
+// $done({body : '[{"key":"iapStatus_v2","value":"Ll/Dvdw/dCzWZev836yv+EYl+ayZXl8Rndw75KjLgpEqXwYAU6iwmR0XgB4lhhZDLzMIDHKjf5GrB/camzGfJk40o05Zod9M/08VW/SPucmMCbHf0PlezL+iwwbCJZY0xZwMoAw+fUougNUcb0TYGXXZc8+Lh7u5oVvlDvqLESe22mLi066CRFqOqwr0YtP+lp52SDqC7TxjSFdcKfO8Hw==","created_at":"3025-03-10T08:19:05.000Z","updated_at":"3025-03-10T14:19:52.000Z"}]'});
