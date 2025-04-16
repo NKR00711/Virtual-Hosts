52667d3238667397deb57b70a16ff3b258053bf7
NKR <14344392+NKR00711@users.noreply.github.com>
2025-04-16 21:55:45 +0530

    Create tableplus_request.js

---
+++ gradle/wrapper/tableplus_request.js
@@ -0,0 +1,17 @@
+function modifyRequest(request) {
+    if (request.url.includes("https://tableplus.com/v1/apps/osx/tableplus") || request.url.includes("https://tableplus.com/v1/licenses/register") || request.url.includes("https://tableplus.com/v1/licenses/devices") || request.url.includes("https://tableplus.com/v1/licenses/devices") || request.url.includes("https://tableplus.com/osx/version.xml")) {
+        // Modify the request headers
+        request.headers["X-Tiny-License-Sign"] = "";
+        delete request.headers["X-Tiny-License-Sign"];
+      delete request.headers["deviceID"];
+      delete request.headers["deviceName"];
+      delete request.headers["platform"];
+      delete request.headers["sign"];
+        const newBody = JSON.stringify({ }); // Create a new body key: "newValue"
+        request.body = newBody; // Set the new body
+    }
+
+    return request;
+}
+
+$httpClient.beforeRequest = modifyRequest;
