d14543a0ac81a5947b2916a52fc978a86fd2da92
NKR <14344392+NKR00711@users.noreply.github.com>
2025-04-16 22:28:35 +0530

    Create tableplus_response.js

---
+++ gradle/wrapper/tableplus_response.js
@@ -0,0 +1,37 @@
+if($response.url.includes("v1/licenses/register")){
+  $response.status = 200;
+  const newBody = JSON.stringify({
+    "Data": {
+    "sign": "12345678901234567890123456789012345678901234567890",
+    "email": "NKR@Over.Lord",  // Replace with the actual email address
+    "deviceID": "1b1b5db57a96712d659770b16be56a93",
+    "licenseKey": "",
+    "purchasedAt": "2025-04-14",
+    "nextChargeAt": 9999,
+    "updatesAvailableUntil": "3024-04-14"
+  },
+  "Code": 200,
+  "Message": "Ok"
+  });
+  $response.body = newBody;
+} else if($response.url.includes("v1/apps/osx/tableplus")){
+  $response.status = 200;
+  var body = $response.body;
+  var obj = JSON.parse(body);
+  obj["Data"]["DayBeforeExpiration"] = 9999;
+  obj["Data"]["LicenseKey"] = "";
+  $response.body = JSON.stringify(obj);
+} else if($response.url.includes("v1/licenses/devices")){
+  $response.status = 200;
+  const newBody = JSON.stringify({
+    "Message": "Ok",  // This field is commented out in the original code
+    "Data": {
+        "DeviceID": "1b1b5db57a96712d659770b16be56a93",
+        "UpdatesAvailableUntilString": "3024-04-14",
+      "updatesAvailableUntil": "3024-04-14"
+    },
+    "Code": 200
+  });
+  $response.body = newBody;
+}
+$done($response);
