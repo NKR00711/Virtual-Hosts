9e3389fb43c595e2a401f5b5ba36201c41b7e04e
NKR <14344392+NKR00711@users.noreply.github.com>
2025-04-16 22:51:24 +0530

    Update tableplus_request.js

--- gradle/wrapper/tableplus_request.js
+++ gradle/wrapper/tableplus_request.js
@@ -1,4 +1,7 @@
-$request.headers["X-Tiny-License-Sign"] = "";
+
+        $request.headers["X-Tiny-License-Sign"] = "";
+	$request.headers["x-tiny-license-sign"] = "";
+delete $request.headers["x-tiny-license-sign"];
         delete $request.headers["X-Tiny-License-Sign"];
       delete $request.headers["deviceID"];
       delete $request.headers["deviceName"];
@@ -6,4 +9,8 @@ $request.headers["X-Tiny-License-Sign"] = "";
       delete $request.headers["sign"];
         const newBody = JSON.stringify({ }); // Create a new body key: "newValue"
         $request.body = newBody; // Set the new body
+if($request.url.includes("v1/licenses/devices")){
+const urlWithoutQuery = $request.url.split('?')[0]; // Get the base URL without query parameters
+        $request.url = urlWithoutQuery;
+}
     $done($request);
