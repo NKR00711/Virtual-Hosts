849ef7b1eb9fa739a0ef27e2b56859cc91fef1fc
NKR <14344392+NKR00711@users.noreply.github.com>
2025-04-16 22:13:16 +0530

    Update tableplus_request.js

--- gradle/wrapper/tableplus_request.js
+++ gradle/wrapper/tableplus_request.js
@@ -1,17 +1,11 @@
-function modifyRequest(request) {
-    if (request.url.includes("https://tableplus.com/v1/apps/osx/tableplus") || request.url.includes("https://tableplus.com/v1/licenses/register") || request.url.includes("https://tableplus.com/v1/licenses/devices") || request.url.includes("https://tableplus.com/v1/licenses/devices") || request.url.includes("https://tableplus.com/osx/version.xml")) {
+if ($request.url.includes("https://tableplus.com/v1/apps/osx/tableplus") || $request.url.includes("https://tableplus.com/v1/licenses/register") || $request.url.includes("https://tableplus.com/v1/licenses/devices") || $request.url.includes("https://tableplus.com/v1/licenses/devices") || $request.url.includes("https://tableplus.com/osx/version.xml")) {
         // Modify the request headers
-        request.headers["X-Tiny-License-Sign"] = "";
-        delete request.headers["X-Tiny-License-Sign"];
-      delete request.headers["deviceID"];
-      delete request.headers["deviceName"];
-      delete request.headers["platform"];
-      delete request.headers["sign"];
+        $request.headers["X-Tiny-License-Sign"] = "";
+        delete $request.headers["X-Tiny-License-Sign"];
+      delete $request.headers["deviceID"];
+      delete $request.headers["deviceName"];
+      delete $request.headers["platform"];
+      delete $request.headers["sign"];
         const newBody = JSON.stringify({ }); // Create a new body key: "newValue"
-        request.body = newBody; // Set the new body
+        $request.body = newBody; // Set the new body
     }
-
-    return request;
-}
-
-$httpClient.beforeRequest = modifyRequest;
