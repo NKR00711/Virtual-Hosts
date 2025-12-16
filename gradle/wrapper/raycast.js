dc2de257e5dd800f55c68f9b9fb06e34470c4125
NKR <14344392+NKR00711@users.noreply.github.com>
2025-12-16 12:28:20 +0530

    Update raycast.js

--- gradle/wrapper/raycast.js
+++ gradle/wrapper/raycast.js
@@ -1,30 +1,20 @@
-let response = $response;
-
-// Check if the response body exists
-if (response && response.body) {
-    var body = JSON.parse(response.body);
-
-    // Modify the body with the provided values
-    body["has_active_subscription"] = true;
-    body["has_pro_features"] = true;
-    body["has_better_ai"] = true;
-    body["publishing_bot"] = true;
-    body["can_upgrade_to_pro"] = false;
-    body["admin"] = true;
-    body["eligible_for_cloud_sync"] = true;
-    body["eligible_for_ai_beta_features"] = true;
-    body["eligible_for_bext"] = true;
-    body["eligible_for_file_search_beta"] = true;
-    body["eligible_for_application_settings"] = true;
-    body["eligible_for_raycast_notes_beta"] = true;
-    body["eligible_for_ai"] = true;
-    body["credits"] = 27163887;
-    body["can_use_referral_codes"] = true;
-    body["has_running_subscription"] = true;
-
-    // Convert back to JSON string
-    response.body = JSON.stringify(body);
-}
-
-// Set the modified response
-$done(response);
+var body = $response.body;
+var obj = JSON.parse(body);
+obj["has_active_subscription"] = true;
+obj["has_pro_features"] = true;
+obj["has_better_ai"] = true;
+obj["publishing_bot"] = true;
+obj["can_upgrade_to_pro"] = false;
+obj["admin"] = true;
+obj["eligible_for_cloud_sync"] = true;
+obj["eligible_for_ai_beta_features"] = true;
+obj["eligible_for_bext"] = true;
+obj["eligible_for_file_search_beta"] = true;
+obj["eligible_for_application_settings"] = true;
+obj["eligible_for_raycast_notes_beta"] = true;
+obj["eligible_for_ai"] = true;
+obj["credits"] = 27163887;
+obj["can_use_referral_codes"] = true;
+obj["has_running_subscription"] = true;
+$response.body = JSON.stringify(obj);
+$done({ body: body });
