1efe3aa8d8f4c6fa72d8251d062db343632d38eb
NKR <14344392+NKR00711@users.noreply.github.com>
2026-01-02 01:45:25 +0530

    Update trakt_infuse_FreeIDMZone.js

--- gradle/wrapper/trakt_infuse_FreeIDMZone.js
+++ gradle/wrapper/trakt_infuse_FreeIDMZone.js
@@ -36,10 +36,10 @@ obj["limits"]["collection"]["item_count"] = 9999999;
 obj["limits"]["notes"]["item_count"] = 9999999;
 obj["limits"]["recommendations"]["item_count"] = 9999999;
 
-obj["data"] = obj["data"] || {};
-obj["data"]["isPro"] = true;
-obj["data"]["plan"] = "Lifetime";
-obj["data"]["expireTime"] = "0";
+// obj["data"] = obj["data"] || {};
+// obj["data"]["isPro"] = true;
+// obj["data"]["plan"] = "Lifetime";
+// obj["data"]["expireTime"] = "0";
 
 body = JSON.stringify(obj);
 $done({ body: body });
