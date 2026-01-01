638c818259b8978934359834873d3307ec0ddafc
NKR <14344392+NKR00711@users.noreply.github.com>
2026-01-02 01:49:44 +0530

    Create ultra_coding.js

---
+++ gradle/wrapper/ultra_coding.js
@@ -0,0 +1,53 @@
+/*************************************
+
+Project name: https://api.ultra-coding.com/login/get-user-info
+Download address: https://t.me/FreeIDMZoneC
+Update date: 2025-04-10
+Script author: NKR
+Telegram channel: https://t.cn/FreeIDMZoneC
+Usage statement: ⚠️For reference only, 🈲Reprint and sale!
+
+**************************************
+
+[rewrite_local]
+https://api.ultra-coding.com/login/get-user-info url script-response-body https://raw.githubusercontent.com/NKR00711/Virtual-Hosts/refs/heads/master/gradle/wrapper/trakt_infuse_FreeIDMZone.js
+
+[mitm]
+hostname = api.ultra-coding.com
+
+*************************************/
+var body = $response.body;
+// var obj = JSON.parse(body);
+
+// obj["vip"] = true;
+
+// obj["user"] = obj["user"] || {}; // Ensure the user object exists
+// obj["user"]["vip_og"] = true;
+// obj["user"]["vip_years"] = 99999;
+// obj["user"]["username"] = "NKR🇮🇳";
+// obj["user"]["name"] = "NKR🇮🇳";
+// obj["user"]["vip"] = true;
+
+// obj["limits"] = obj["limits"] || {};
+// obj["limits"]["list"]["item_count"] = 9999999;
+// obj["limits"]["watchlist"]["item_count"] = 9999999;
+// obj["limits"]["favorites"]["item_count"] = 9999999;
+// obj["limits"]["collection"]["item_count"] = 9999999;
+// obj["limits"]["notes"]["item_count"] = 9999999;
+// obj["limits"]["recommendations"]["item_count"] = 9999999;
+
+// obj["data"] = obj["data"] || {};
+obj["data"]["isPro"] = true;
+obj["data"]["plan"] = "Lifetime";
+obj["data"]["expireTime"] = "0";
+
+// body = JSON.stringify(obj);
+$done({ body: body });
+
+// var ddm = JSON.parse($response.body);
+
+// ddm=[{"key":"iapStatus_v2","value":"Ll/Dvdw/dCzWZev836yv+EYl+ayZXl8Rndw75KjLgpEqXwYAU6iwmR0XgB4lhhZDLzMIDHKjf5GrB/camzGfJk40o05Zod9M/08VW/SPucmMCbHf0PlezL+iwwbCJZY0xZwMoAw+fUougNUcb0TYGXXZc8+Lh7u5oVvlDvqLESe22mLi066CRFqOqwr0YtP+lp52SDqC7TxjSFdcKfO8Hw==","created_at":"3025-03-10T08:19:05.000Z","updated_at":"3025-03-10T14:19:52.000Z"}];
+
+// $done({body : JSON.stringify(ddm)});
+
+// $done({body : '[{"key":"iapStatus_v2","value":"Ll/Dvdw/dCzWZev836yv+EYl+ayZXl8Rndw75KjLgpEqXwYAU6iwmR0XgB4lhhZDLzMIDHKjf5GrB/camzGfJk40o05Zod9M/08VW/SPucmMCbHf0PlezL+iwwbCJZY0xZwMoAw+fUougNUcb0TYGXXZc8+Lh7u5oVvlDvqLESe22mLi066CRFqOqwr0YtP+lp52SDqC7TxjSFdcKfO8Hw==","created_at":"3025-03-10T08:19:05.000Z","updated_at":"3025-03-10T14:19:52.000Z"}]'});
